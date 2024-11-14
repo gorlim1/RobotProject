09b34c57dcd1fb239ce64ce418bfe876a7dc70be
Desmond Roche <desmondroche@gmail.com>
2024-11-14 13:05:04 -0500

    Testing commit process

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Robot myRobot = new Robot(); //Create a new Robot object
+
+        myRobot.move(); //Make the robot move
+        myRobot.checkBatteryLevel(); //Check battery level
+        myRobot.stop(); //Stop the robot
+        myRobot.chargeBattery(); //Charge the robot's battery
+        myRobot.displayInfo(); //Display the robot's OS Version, weight, and color
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Robot {
+
+    int batteryLevel = 100; //Battery level percentage (0 to 100)
+    int speed = 0; //Current speed of the robot
+    double osVersion = 2.17; //Version of robot's OS
+    double lbsWeight = 3.6; //Weight of the robot
+    String name = "Robo"; //Name of the Robot
+    String color = "blue"; //Color of the robot
+
+    public void move() {
+        speed = 5;
+        System.out.println(name + " is moving at speed " + speed + " mph.");
+    }
+
+    public void stop() {
+        speed = 0;
+        System.out.println(name + " has stopped.");
+    }
+
+    public void chargeBattery() {
+        batteryLevel = 100;
+        System.out.println(name + "'s battery is fully charged.");
+    }
+
+    public void checkBatteryLevel() {
+        System.out.println(name + "'s current battery level is: " + batteryLevel + "%.");
+    }
+
+    public void displayInfo() {
+        System.out.println(name + "'s OS Version, weight, and color are " + osVersion + ", " + lbsWeight + " lb, and " + color + ".");
+    }
 }
